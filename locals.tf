a02a1787002cd4bc9c18bca6d8e495d894c2d97e
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-19 21:30:48 +0530

    Terraform VPC Module

--- locals.tf
+++ locals.tf
@@ -6,4 +6,5 @@ locals {
   }
 
   common_name_suffix = "${var.project_name}-${var.environment}"  # roboshop-qa
+  az_names = slice(data.aws_availability_zones.available.names, 0, 2 )
 }
--- variables.tf
+++ variables.tf
@@ -15,3 +15,31 @@ variable "vpc_tags" {
   type = map
   default = {}
 }
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidrs" {
+  default = {}
+}
+
+variable "public_subnet_tags" {
+  default = {}
+}
+
+variable "private_subnet_cidrs" {
+  default = {}
+}
+
+variable "private_subnet_tags" {
+  default = {}
+}
+
+variable "database_subnet_cidrs" {
+  default = {}
+}
+
+variable "database_subnet_tags" {
+  default = {}
+}
