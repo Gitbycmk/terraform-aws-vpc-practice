61ea9046664fc224ee3e9a4f701d3cc58ea62a03
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-21 19:48:59 +0530

    Terraform VPC Module

--- vpc.tf
+++ vpc.tf
@@ -117,3 +117,30 @@ resource "aws_route_table" "database" {
     }
   )
 }
+
+## Route
+# Public Route
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
